de971afb3d4214dee5665f6323693caa2273525b
kangwei <kangwei@163.com>
2019-04-16 17:24:59 +0800

    Create ListAct File

---
+++ src/main/java/org/kt/temp/action/ListAct.java
@@ -0,0 +1,12 @@
+package org.kt.temp.action;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListAct {
+
+
+    public String hello() {
+        return "Hello";
+    }
+}
